7a1c3d227dd7a6ee66c1aa0fc8621e80b1f14348
samahhamed <>
2021-06-02 12:27:13 +0000

    new html file

---
+++ app.js
@@ -0,0 +1,17 @@
+var x = "this website contains food"
+alert ( x)
+var  userAnswer = prompt("which type of food do you like?")
+alert('you like ' +userAnswer )
+console.log(userAnswer)
+document.write('<div>' + 'Welcome to our kitchen' + "</div>")
+
+
+if (userAnswer == 'mansaf') {
+  document.write('<h3>' + userAnswer + '</h3>')
+}
+else   {
+  document.write('<h3>' + userAnswer + '</h3>')
+
+}
+
+
