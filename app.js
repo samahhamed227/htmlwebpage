04e4d595f454686b4ed2d4dfaea03950d85d1d9d
samahhamed <>
2021-06-03 14:14:34 +0000

    new5

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
-/*var x = "this website contains food"
+var x = "this website contains food"
 alert ( x)
-var  userAnswer = prompt("which type of food do you like?")
-alert('you like ' +userAnswer )
-console.log(userAnswer)
+var Answer = prompt("what is  food do you like?")
+alert('you like ' +Answer )
+//var  userAnswer = prompt("how many food do you like?")
 document.write('<div>' + 'Welcome to our kitchen' + "</div>")
 
+var userInput = prompt("how many  meals do you want to require?")
 
-if (userAnswer == 'mansaf') {
-  document.write('<h3>' + userAnswer + '</h3>')
+
+
+
+if (userInput<10)
+
+
+ {for (var i =1; i<userInput; i++)
+
+{
+  document.write( 'it is a'+'<h3>' + i+ 'meal'+'</h3>' )
 }
-else   {
-  document.write('<h3>' + userAnswer + '</h3>')
+  document.write( 'you are welcom we have a'+'<h3>' +userInput + 'meal'+'</h3>' + '<img src="https://paleomg.com/wp-content/uploads/2018/07/PicMonkey-Collage-1.jpg" width="200px" hieght="200px"/>' )
 
-}
-
+  
 function myFunction() {
-  confirm("Enjoy your meal");
-}*/
+  while(userInput >3  || userInput <10){
+  
+
+confirm("Enjoy your "+ Answer
++ "meal");
+}
+}
+}
+else if (userInput>10){
+
+
+  document.write('  we dont have '+'<h3>' + userInput + '</h3>'+'meals')
+
+}
+
+
+
 
